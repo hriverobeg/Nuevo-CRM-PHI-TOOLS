5754a5196987725b427dace73e3d9f7082556b4c
gl14491 <giovanni.arandaa@gmail.com>
2023-08-01 17:53:34 -0500

    Update wrapper.blade.php

--- resources/views/pages/cotizaciones/wrapper.blade.php
+++ resources/views/pages/cotizaciones/wrapper.blade.php
@@ -132,6 +132,6 @@
         var isAdmin = {{ json_encode($isAdmin) }}
     </script>
     <script src="/assets/js/Sortable.min.js"></script>
-    <script src="/assets/js/cotizaciones.js"></script>
+    <script src="/assets/js/cotizaciones.js?v=1.0.0"></script>
     @vite('resources/js/pdf/main.js')
 @endsection
