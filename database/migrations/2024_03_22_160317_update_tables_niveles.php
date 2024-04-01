b1c2cb80a7f142b686e56bcd44e44eb83d28c11e
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-04-01 16:19:58 -0600

    Update 2024_03_22_160317_update_tables_niveles.php

--- database/migrations/2024_03_22_160317_update_tables_niveles.php
+++ database/migrations/2024_03_22_160317_update_tables_niveles.php
@@ -21,10 +21,16 @@ return new class extends Migration
            FROM admin');
 
 
-       # add all usuarios to user
-         DB::statement('INSERT INTO user (nivel_id, parent_user_id, nombre, email, telefono, empresa, direccion_empresa, giro_empresa, website_empresa, created_at, updated_at, deleted_at)
-              SELECT 3 as nivel_id, admin_id as parent_user_id, nombre, email, telefono, empresa, direccion_empresa, giro_empresa, website_empresa, created_at, updated_at, deleted_at
-              FROM usuario');
+       # add all usuarios to user using
+       # table admin has email and table email has the same email
+         DB::statement('INSERT INTO user (nivel_id, parent_user_id, nombre, email, password, telefono, empresa, created_at, updated_at, deleted_at)
+              SELECT 3 as nivel_id, (SELECT id FROM user WHERE email = a.email LIMIT 1) as parent_user_id,
+              u.nombre, u.email, NULL, u.telefono, u.empresa, u.created_at, u.updated_at, u.deleted_at
+              FROM usuario u
+              LEFT JOIN admin a ON a.id = u.admin_id');
+        //  DB::statement('INSERT INTO user (nivel_id, parent_user_id, nombre, email, telefono, empresa, direccion_empresa, giro_empresa, website_empresa, created_at, updated_at, deleted_at)
+        //       SELECT 3 as nivel_id, admin_id as parent_user_id, nombre, email, telefono, empresa, direccion_empresa, giro_empresa, website_empresa, created_at, updated_at, deleted_at
+        //       FROM usuario');
 
         # add new column from_user_id and foreign key to cotizacion with DB:statement
         DB::statement('ALTER TABLE cotizacion ADD COLUMN from_user_id BIGINT UNSIGNED AFTER id');
