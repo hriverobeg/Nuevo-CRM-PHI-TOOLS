2e1fb5e16615f3a912deddd48df3ebd40ac97f57
Giovanni Aranda <giovanni.arandaa@gmail.com>
2024-06-05 16:43:58 -0600

    Ultimas mejoras

--- public/assets/js/table-cotizacion.js
+++ public/assets/js/table-cotizacion.js
@@ -68,14 +68,20 @@ const columnPDF = (select) => ({
     }
 })
 
+function getNumbersBeforeDash(str) {
+  const match = str.match(/^(\d+)-/);
+  return match ? match[1] : null;
+}
+
 const columnPDFGrupo = (select) => ({
     select,
     sortable: false,
     render: (data, cell, row) => {
         if (String(data).endsWith('null')) return null;
+        const id = getNumbersBeforeDash(data)
 
         return `<div class="flex items-center">
-            <button x-disabled="isLoadingPdf" class="btn btn-primary btn-sm" @click="downloadPdfTable(${data}, true)">
+            <button x-disabled="isLoadingPdf" class="btn btn-primary btn-sm" @click="downloadPdfTable(${id}, true)">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
                     <path opacity="0.5" d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                     <path d="M12 3V16M12 16L16 11.625M12 16L8 11.625" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -161,7 +167,6 @@ const columnDetails = (select = 4) => ({
         if (grupo) {
           this.isLoadingGrupoPdf = true
           const cotizaciones = this.cotizaciones.filter(f => f.grupo === this.row?.grupo)
-
           await window.createPDF(cotizaciones, {
               nombre: this.row?.to_user?.nombre,
               empresa: this.row?.to_user?.empresa ?? '',
@@ -177,7 +182,9 @@ const columnDetails = (select = 4) => ({
         }
       },
       downloadPdfTable(id, grupo = false) {
-        this.row = dataLaravel.find((f) => f.id === id);
+        const row = dataLaravel.find((f) => f.id === id);
+        this.row = row
+
         this.downloadPdf(grupo);
       },
       onDelete(id) {
